22c9e7a17ac4f3865c636db6efc936fa0121a0ae
Hector <124hmunoz@frhsd.com>
2024-01-04 10:59:50 -0500

    good

--- gallery.js
+++ gallery.js
@@ -26,10 +26,82 @@ fetchJSON(jsonUrl, function (data) {
     console.log(data);
 });
 
-Function fetchJSON(){
-    mRequest.onreadystatechange = function() {
-        if (this.readystate == 4 && this.status === 2000) {
 
-        }
-    }
+function swapPhoto() {
+    document.getElementById('photo').src = mimages[mCurrentindex].url;
+
+    console.log('swap photo');
 }
+
+function swapPhoto() {
+    document.getElementById('photo').src = mImages[mCurrentIndex].url;
+    document.getElementsByClassName[0].innerHTML = mImages[0].innerHtml
+}
+
+
+
+
+
+var mRequest = new XMLHttpRequest();
+function fetchJSON()
+{
+    mRequest.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            = mRequest.responseText;
+        }
+    };
+    mRequest.open("GET", mURl, true);
+    mRequest.send();
+}
+
+
+
+function GalleryImage() {
+
+let location;
+
+let description;
+
+let date;
+
+let url;
+}-
+
+iterateJSON()
+{
+    for(let i = 0; i < mjson.images.length; i++)
+{
+    mImages[i] = new GalleryImage();
+    mImages[i].location = mJson.images.imgLocation;
+    mImages[i].descrption = mJson.images.imgLocation;
+    mImages[i].date = mJson.images.imgLocation;
+    mImages[i].url = mJson.images.imgLocation;
+}
+}
+
+if(
+    mCurrentIndex >= mJson.images.length
+) {
+    mCurrentIndex = 0
+}
+
+function GalleryImage(location, description, date, img) {
+    this.location = location;
+    this.description = description;
+    this.date = date;
+    this.img = img;
+  }
+  
+  // Example usage
+  var myGalleryImage = new GalleryImage(
+    "City Park",
+    "Beautiful sunset in the park",
+    "2024-01-04",
+    "https://example.com/sunset.jpg"
+  );
+  
+  // Accessing properties
+  console.log("Location: " + myGalleryImage.location);
+  console.log("Description: " + myGalleryImage.description);
+  console.log("Date: " + myGalleryImage.date);
+  console.log("Image URL: " + myGalleryImage.img);
