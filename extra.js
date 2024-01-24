e273c2d68c19f28f0ea87ba115ad608665287a85
Hector <124hmunoz@frhsd.com>
2024-01-24 09:44:03 -0500

    done deal

---
+++ extra.js
@@ -0,0 +1,16 @@
+{
+    "images": [
+      {
+        "imgPath": "img/places/pizza.png",
+            "imgLocation": "Resturant",
+            "description": "Delicious Pizza",
+            "date": "01/01/2016"
+      },
+      {
+        "imgPath": "img/places/burger.png",
+            "imgLocation": "Resturant",
+            "description": "Delicious Burger",
+            "date": "01/01/2016"
+      }
+    ]
+  }
